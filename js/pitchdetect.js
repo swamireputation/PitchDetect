38fec7900a63d6464c3b44ad8a98af547d6272a9
Swami Venkataramani <svenkataramani@reputation.com>
2021-09-24 11:01:14 -0700

    code cleanup

--- js/pitchdetect.js
+++ js/pitchdetect.js
@@ -1,27 +1,3 @@
-/*
-The MIT License (MIT)
-
-Copyright (c) 2014 Chris Wilson
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
-
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
 var frequencies = [];
 var audioContext = null;
@@ -29,68 +5,60 @@ var isPlaying = false;
 var sourceNode = null;
 var analyser = null;
 var theBuffer = null;
-var DEBUGCANVAS = null;
+var canvasElem = null;
 var mediaStreamSource = null;
 var detectorElem, 
-	canvasElem,
-	waveCanvas,
+	canvas,
 	pitchElem,
-	noteElem,
-	detuneElem,
-	detuneAmount;
+	noteElem;
+var rafID = null;
+var tracks = null;
+var buflen = 2048;
+var buf = new Float32Array(buflen);
+
+var noteStrings = [
+    "C",
+    "C#",
+    "D",
+    "D#",
+    "E",
+    "F",
+    "F#",
+    "G",
+    "G#",
+    "A",
+    "A#",
+    "B",
+];
 
 window.onload = function() {
 	audioContext = new AudioContext();
-	MAX_SIZE = Math.max(4,Math.floor(audioContext.sampleRate/5000));	// corresponds to a 5kHz signal
-	var request = new XMLHttpRequest();
-	request.open("GET", "../sounds/whistling3.ogg", true);
-	request.responseType = "arraybuffer";
-	request.onload = function() {
-	  audioContext.decodeAudioData( request.response, function(buffer) { 
-	    	theBuffer = buffer;
-		} );
-	}
-	request.send();
-
 	detectorElem = document.getElementById( "detector" );
-	canvasElem = document.getElementById( "output" );
-	DEBUGCANVAS = document.getElementById( "waveform" );
-	if (DEBUGCANVAS) {
-		waveCanvas = DEBUGCANVAS.getContext("2d");
-		waveCanvas.strokeStyle = "black";
-		waveCanvas.lineWidth = 1;
+	canvasElem = document.getElementById("canvas");
+	if (canvasElem) {
+		canvas = canvasElem.getContext("2d");
+		canvas.strokeStyle = "black";
+		canvas.lineWidth = 1;
 	}
 	pitchElem = document.getElementById( "pitch" );
 	noteElem = document.getElementById( "note" );
-	detuneElem = document.getElementById( "detune" );
-	detuneAmount = document.getElementById( "detune_amt" );
-
-	detectorElem.ondragenter = function () { 
-		this.classList.add("droptarget"); 
-		return false; };
-	detectorElem.ondragleave = function () { this.classList.remove("droptarget"); return false; };
-	detectorElem.ondrop = function (e) {
-  		this.classList.remove("droptarget");
-  		e.preventDefault();
-		theBuffer = null;
-
-	  	var reader = new FileReader();
-	  	reader.onload = function (event) {
-	  		audioContext.decodeAudioData( event.target.result, function(buffer) {
-	    		theBuffer = buffer;
-	  		}, function(){alert("error loading!");} ); 
-
-	  	};
-	  	reader.onerror = function (event) {
-	  		alert("Error: " + reader.error );
-		};
-	  	reader.readAsArrayBuffer(e.dataTransfer.files[0]);
-	  	return false;
-	};
 }
 
-function error() {
-    alert('Stream generation failed.');
+function startHumming() {
+    getUserMedia(
+        {
+            audio: {
+                mandatory: {
+                    googEchoCancellation: "false",
+                    googAutoGainControl: "false",
+                    googNoiseSuppression: "false",
+                    googHighpassFilter: "false",
+                },
+                optional: [],
+            },
+        },
+        gotStream
+    );
 }
 
 function getUserMedia(dictionary, callback) {
@@ -116,49 +84,50 @@ function gotStream(stream) {
     updatePitch();
 }
 
-function toggleLiveInput() {
-    if (isPlaying) {
-        //stop playing and return
-        sourceNode.stop( 0 );
-        sourceNode = null;
-        analyser = null;
-        isPlaying = false;
-		if (!window.cancelAnimationFrame)
-			window.cancelAnimationFrame = window.webkitCancelAnimationFrame;
-        window.cancelAnimationFrame( rafID );
+function updatePitch(time) {
+    var cycles = new Array();
+    analyser.getFloatTimeDomainData(buf);
+    var pitch = autoCorrelate(buf, audioContext.sampleRate);
+
+    if (canvasElem) {
+        // This draws the current waveform, useful for debugging
+        canvas.clearRect(0, 0, 512, 256);
+        canvas.strokeStyle = "red";
+        canvas.beginPath();
+        canvas.moveTo(0, 0);
+        canvas.lineTo(0, 256);
+        canvas.moveTo(128, 0);
+        canvas.lineTo(128, 256);
+        canvas.moveTo(256, 0);
+        canvas.lineTo(256, 256);
+        canvas.moveTo(384, 0);
+        canvas.lineTo(384, 256);
+        canvas.moveTo(512, 0);
+        canvas.lineTo(512, 256);
+        canvas.stroke();
+        canvas.strokeStyle = "black";
+        canvas.beginPath();
+        canvas.moveTo(0, buf[0]);
+        for (var i = 1; i < 512; i++) {
+            canvas.lineTo(i, 128 + buf[i] * 128);
+        }
+        canvas.stroke();
     }
-    getUserMedia(
-    	{
-            "audio": {
-                "mandatory": {
-                    "googEchoCancellation": "false",
-                    "googAutoGainControl": "false",
-                    "googNoiseSuppression": "false",
-                    "googHighpassFilter": "false"
-                },
-                "optional": []
-            },
-        }, gotStream);
+
+    if (pitch != -1) {
+        pitchElem.innerText = Math.round(pitch);
+        var note = noteFromPitch(pitch);
+        noteElem.innerHTML = noteStrings[note % 12];
+    }
+
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = window.webkitRequestAnimationFrame;
+    rafID = window.requestAnimationFrame(updatePitch);
 }
 
-var rafID = null;
-var tracks = null;
-var buflen = 2048;
-var buf = new Float32Array( buflen );
-
-var noteStrings = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
-
-function noteFromPitch( frequency ) {
+function noteFromPitch(frequency) {
 	var noteNum = 12 * (Math.log( frequency / 440 )/Math.log(2) );
 	return Math.round( noteNum ) + 69;
-}
-
-function frequencyFromNoteNumber( note ) {
-	return 440 * Math.pow(2,(note-69)/12);
-}
-
-function centsOffFromPitch( frequency, note ) {
-	return Math.floor( 1200 * Math.log( frequency / frequencyFromNoteNumber( note ))/Math.log(2) );
 }
 
 function autoCorrelate( buf, sampleRate ) {
@@ -206,62 +175,7 @@ function autoCorrelate( buf, sampleRate ) {
 	return sampleRate/T0;
 }
 
-function updatePitch( time ) {
-	var cycles = new Array;
-	analyser.getFloatTimeDomainData( buf );
-	var ac = autoCorrelate( buf, audioContext.sampleRate );
-	// TODO: Paint confidence meter on canvasElem here.
-
-	if (DEBUGCANVAS) {  // This draws the current waveform, useful for debugging
-		waveCanvas.clearRect(0,0,512,256);
-		waveCanvas.strokeStyle = "red";
-		waveCanvas.beginPath();
-		waveCanvas.moveTo(0,0);
-		waveCanvas.lineTo(0,256);
-		waveCanvas.moveTo(128,0);
-		waveCanvas.lineTo(128,256);
-		waveCanvas.moveTo(256,0);
-		waveCanvas.lineTo(256,256);
-		waveCanvas.moveTo(384,0);
-		waveCanvas.lineTo(384,256);
-		waveCanvas.moveTo(512,0);
-		waveCanvas.lineTo(512,256);
-		waveCanvas.stroke();
-		waveCanvas.strokeStyle = "black";
-		waveCanvas.beginPath();
-		waveCanvas.moveTo(0,buf[0]);
-		for (var i=1;i<512;i++) {
-			waveCanvas.lineTo(i,128+(buf[i]*128));
-		}
-		waveCanvas.stroke();
-	}
-
- 	if (ac == -1) {
- 		detectorElem.className = "vague";
-	 	pitchElem.innerText = "--";
-		noteElem.innerText = "-";
-		detuneElem.className = "";
-		detuneAmount.innerText = "--";
- 	} else {
-	 	detectorElem.className = "confident";
-	 	pitch = ac;
-	 	pitchElem.innerText = Math.round( pitch ) ;
-	 	var note =  noteFromPitch( pitch );
-		noteElem.innerHTML = noteStrings[note%12];
-		var detune = centsOffFromPitch( pitch, note );
-		if (detune == 0 ) {
-			detuneElem.className = "";
-			detuneAmount.innerHTML = "--";
-		} else {
-			if (detune < 0)
-				detuneElem.className = "flat";
-			else
-				detuneElem.className = "sharp";
-			detuneAmount.innerHTML = Math.abs( detune );
-		}
-	}
-
-	if (!window.requestAnimationFrame)
-		window.requestAnimationFrame = window.webkitRequestAnimationFrame;
-	rafID = window.requestAnimationFrame( updatePitch );
+function error() {
+    alert("Stream generation failed.");
 }
+
